e8eec1543e7d270b77157e29cea5f11b1df30e0b
njenkins <web@nathanjenkins.com.au>
2016-04-11 07:39:54 +1000

    Test for duplicate keys

--- GruntFile.js
+++ GruntFile.js
@@ -12,5 +12,28 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-lintspaces');
 
-  grunt.registerTask('default', ['lintspaces']);
+  //Ensure that all snippet shortcuts are unique.
+  grunt.registerTask('prefixconflict', function(){
+    var snippetFiles = grunt.file.expand('snippets/**/*');
+    var prefixKeys = [];
+    snippetFiles.forEach(function(snippetFilePath){
+      //If path is to a snippet config file get file content
+      if(snippetFilePath.indexOf('.cson') !== -1){
+        var snippetFileContent = grunt.file.read(snippetFilePath);
+        var asArray = snippetFileContent.split('\n');
+        if(typeof asArray[2] !== 'undefined'){
+          var prefixKey = asArray[2].split(':')[1].trim();
+          if(prefixKeys.indexOf(prefixKey) == -1){
+            prefixKeys.push(prefixKey);
+          }
+          else {
+            grunt.log.error(prefixKey + 'already exists');
+            return false;
+          }
+        }
+
+      }
+    });
+  })
+  grunt.registerTask('default', ['prefixconflict','lintspaces']);
 };
